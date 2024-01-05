ca566423e42075031a8c9b801f5871ac23706490
easy <86452817+easyuxd@users.noreply.github.com>
2024-01-04 21:54:47 -0800

    Icons v1.2 - webfonts

---
+++ orangeclock-icons_icons.dart
@@ -0,0 +1,30 @@
+// Place fonts/orangeclock-icons.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: orangeclock-icons
+//      fonts:
+//       - asset: fonts/orangeclock-icons.ttf
+import 'package:flutter/widgets.dart';
+
+class Orangeclock_icons {
+  Orangeclock_icons._();
+
+  static const String _fontFamily = 'orangeclock-icons';
+
+  static const IconData block = IconData(0x41, fontFamily: _fontFamily);
+  static const IconData euro = IconData(0x42, fontFamily: _fontFamily);
+  static const IconData fees = IconData(0x43, fontFamily: _fontFamily);
+  static const IconData globe = IconData(0x44, fontFamily: _fontFamily);
+  static const IconData hourglass = IconData(0x45, fontFamily: _fontFamily);
+  static const IconData lightning = IconData(0x46, fontFamily: _fontFamily);
+  static const IconData loading = IconData(0x47, fontFamily: _fontFamily);
+  static const IconData sats = IconData(0x48, fontFamily: _fontFamily);
+  static const IconData sat_usd = IconData(0x49, fontFamily: _fontFamily);
+  static const IconData settings = IconData(0x4a, fontFamily: _fontFamily);
+  static const IconData usd = IconData(0x4b, fontFamily: _fontFamily);
+  static const IconData wifi = IconData(0x4c, fontFamily: _fontFamily);
+  static const IconData messaging_error = IconData(0x4d, fontFamily: _fontFamily);
+  static const IconData messaging_success = IconData(0x4e, fontFamily: _fontFamily);
+  static const IconData messaging_warning = IconData(0x4f, fontFamily: _fontFamily);
+}
